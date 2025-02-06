317de39115ab8109149242fb57a4aafd919cdf70
kheireddineGuidara <kheireddineguidara@gmail.com>
2025-02-06 09:19:09 +0100

    Adding ArticleAuthorResponseDto .

---
+++ src/Dto/ArticleAuthorResponseDto.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Dto;
+class ArticleAuthorResponseDto
+{
+    private ?string $title=null;
+    private ?string $author=null;
+
+    public function __construct()
+    {
+    }
+    public function getTitle()
+    {
+        return $this->title;
+    }
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+}
